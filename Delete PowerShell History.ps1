9387136969b831544e9d02f6f25830854d3da204
sidr-champlain <sidr.champlain@protonmail.com>
2022-05-12 17:28:26 -0400

    Updated powershell-files

--- Delete PowerShell History.ps1
+++ Delete PowerShell History.ps1
@@ -1,10 +1,11 @@
 ﻿# Delete PowerShell History 
 
-cd \Users\pilfu\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\
+cd \Users\[User]\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\
 
 rm .\ConsoleHost_history.txt
 
 # If running in the console, wait for input before closing.
+
 if ($Host.Name -eq "ConsoleHost")
 {
     Write-Host "Press any key to continue..."
